405cfb2d3437ef16b9fdc5dbf7271d204a21b7a2
nick <nikhilnick00007@gmail.com>
2019-04-08 07:55:25 +0000

    rolex: xenon bringup

--- xenonhd_rolex.mk
+++ xenonhd_rolex.mk
@@ -17,12 +17,12 @@
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common.mk)
 
 #Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := xenonhd_rolex
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
